bd4f7cc29660a5e84b13b8469ffacd1e48e66088
Christopher Dembia <cld72@cornell.edu>
2013-05-21 22:28:57 -0700

    Created executable to generate tool serialization template.

---
+++ optimizer_tool_template.cpp
@@ -0,0 +1,29 @@
+
+#include <iostream>
+
+#include <FelisCatusOptimizerSystem.h>
+
+using std::cout;
+using std::endl;
+using std::string;
+
+using OpenSim::FelisCatusOptimizerTool;
+
+/**
+ * Prints a template XML serialization of a FelisCatusOptimizerTool.
+ * */
+int main(int argc, char * argv[])
+{
+    string filename = "feliscatusoptimizertool_template.xml";
+    FelisCatusOptimizerTool tool;
+    tool.set_results_directory("results");
+    tool.set_model_filename("");
+    tool.set_duration(1.0);
+    tool.set_num_optim_spline_points(5);
+    tool.set_initial_parameters_filename("");
+    tool.print(filename);
+
+    cout << "Printed template as " << filename << "." << endl;
+
+    return EXIT_SUCCESS;
+}
