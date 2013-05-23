d4fcd45188c979a9bd4e0c838479cddc7712ef96
Sean Sketch <sean.sketch@gmail.com>
2013-05-23 11:24:32 -0700

    Model tree READY!

--- FelisCatusModeling.h
+++ FelisCatusModeling.h
@@ -13,6 +13,7 @@ using std::vector;
 using OpenSim::Array;
 using OpenSim::Body;
 using OpenSim::CoordinateCouplerConstraint;
+using OpenSim::Coordinate;
 using OpenSim::CoordinateSet;
 using OpenSim::DisplayGeometry;
 using OpenSim::LinearFunction;
@@ -52,20 +53,18 @@ public:
         // Create the model.
         cat = Model();
         cat.setName(modelName);
-        // TODO cat.setGravity(Vec3(0, -9.81, 0));
-        cat.setGravity(Vec3(0, 0, 0));
+        cat.setGravity(Vec3(0, -9.81, 0));
 
         // Constants/parameters.
-        segmentalLength = 0.2;			// m
-        segmentalDiam = 0.15;			// m
-        segmentalMass = 1;				// kg
-		legsLength = 0.12;				// m
-		legsDiam = 0.1*legsLength;		// m
-		legsWidth = 0.6* segmentalDiam; // m; sum of both legs (60% distance across the belly)
-		legsMass = 0.2;					// kg
-        // See Kane and Scher (1969) about next 3 lines:
+        segmentalLength = 0.175;		 // m
+        segmentalDiam = 0.15;			 // m
+        segmentalMass = 1;				 // kg
+		legsLength = 0.125;				 // m
+		legsDiam = 0.1 * legsLength;	 // m
+		legsWidth = 0.6 * segmentalDiam; // m; sum of both legs (60% distance across the belly)
+		legsMass = 0.2;					 // kg
         I = 1; // kg-m^2; transverse moment of inertia
-        JIratio = 0.25;
+        JIratio = 0.25; // from Kane and Scher (1969)
         double J = JIratio * I; // axial moment of inertia
         double Ixx = J; // + mass*(bodyLength/2.0)*(bodyLength/2.0); // kg-m^2, shifted because of displaced joint TODO might be unneccessary? I is about COM.
         double Iyy = I; // kg-m^2
@@ -74,19 +73,11 @@ public:
         double Ixz = 0; // kg-m^2
         double Iyz = 0; // kg-m^2
         segmentalInertia = Inertia(Ixx, Iyy, Izz, Ixz, Ixz, Iyz);
-		legsInertia = Inertia(I, I, I, 0, 0, 0); // TODO
+		legsInertia = (legsMass/segmentalMass) * segmentalInertia;
         zeroInertia = Inertia(0, 0, 0, 0, 0, 0);
 
-        // Call the 'add' methods below (NOTE: the order of these calls matters, in general)
         addBodies();
         addDisplayGeometry();
-        addJoints();
-		addContactGeometry();
-		addContactForces();
-        addCoordinateLimitForces();
-        addActuators();
-
-        cat.print(fileName);
     }
 
 protected:
@@ -113,7 +104,7 @@ protected:
     Inertia zeroInertia;
 
     // Body construction and mass properties. This function does not include
-    // massless bodies necessary to define a joint type.
+    // massless bodies necessary to define some joint types.
     void addBodies()
     {
         // Anterior half of cat.
@@ -144,57 +135,19 @@ protected:
         posteriorLegs->setInertia(legsInertia);
     }
 
-     // This method is responsible for adding anteriorBody and posteriorBody to
-     // the model.
-    virtual void addJoints() = 0;
-
-     // For help with creating massless Body's (frames). If we weren't simply
-     // building a model, creating a pointer like this wouldn't be a good
-     // idea.
+    // For help with creating joints that require intermediate frames
+	// (i.e., massless bodies).
     Body * newMasslessBody(string name)
     {
         Body * massless = new Body();
         massless->setName(name);
-        massless->setMass(0.0);
-        massless->setMassCenter(Vec3(0.0, 0.0, 0.0));
+        massless->setMass(0);
+        massless->setMassCenter(Vec3(0));
         massless->setInertia(zeroInertia);
         return massless;
     }
-
-    // Adds contact geometry for when cat hits the ground.
-	virtual void addContactGeometry()
-	{
-		// TODO - do we really want this?
-	}
-
-    // Adds contact forces to act when contact geometries come
-	// into contact.
-	virtual void addContactForces()
-	{
-		// TODO - do we really want this?
-	}
-
-    // Resist joint motions past their limits.
-    virtual void addCoordinateLimitForces()
-    {
-        // TODO - do we really want this?
-    }
-
-    // Adds actuators to degrees of freedom allowed by joints.
-	virtual void addActuators() {
-		// TODO
-		
-		/*
-		TorqueActuator * testActuator = new TorqueActuator("anteriorBody",
-        "posteriorBody");
-        testActuator->setTorqueIsGlobal(false);
-        testActuator->setAxis(Vec3(1.0, 0.0, 0.0));
-        double optimalForce = 250.0;
-        testActuator->setOptimalForce(optimalForce);
-		*/
-	};
     
-    // Adds display geometry to anteriorBody and posteriorBody.
+    // Adds display geometry to all bodies.
     void addDisplayGeometry()
     {
         Body & ground = cat.getGroundBody();
@@ -204,55 +157,42 @@ protected:
 
         // Anterior body.
         DisplayGeometry * anteriorDisplay = 
-            new DisplayGeometry("sphere.vtp");
-		//            "feliscatus_cylinder_with_two_offset_feet_nubs.obj");
+            new DisplayGeometry("cylinder.vtp");
         anteriorDisplay->setOpacity(0.5);
         anteriorDisplay->setColor(Vec3(0.5, 0.5, 0.5));
         Rotation rot;
-        rot.setRotationFromAngleAboutY(Pi);
-        anteriorDisplay->setTransform(Transform(rot));
-
-        anteriorBody->updDisplayer()->updGeometrySet().adoptAndAppend(
-                anteriorDisplay);
+        rot.setRotationFromAngleAboutZ(0.5 * Pi);
+        anteriorDisplay->setTransform(Transform(rot, Vec3(-0.5 * segmentalLength, 0, 0)));
+		anteriorDisplay->setScaleFactors(Vec3(segmentalDiam, segmentalLength, segmentalDiam));
+        anteriorBody->updDisplayer()->updGeometrySet().adoptAndAppend(anteriorDisplay);
         anteriorBody->updDisplayer()->setShowAxes(true);
-		anteriorBody->updDisplayer()->setScaleFactors(
-                Vec3(segmentalLength, segmentalDiam, segmentalDiam));
 
         // Posterior body.
         DisplayGeometry * posteriorDisplay = 
-            new DisplayGeometry("sphere.vtp");
-		//            "feliscatus_cylinder_with_two_offset_feet_nubs.obj");
+            new DisplayGeometry("cylinder.vtp");
         posteriorDisplay->setOpacity(0.5);
         posteriorDisplay->setColor(Vec3(0.7, 0.7, 0.7));
-
-        posteriorBody->updDisplayer()->updGeometrySet().adoptAndAppend(
-                posteriorDisplay);
+		posteriorDisplay->setTransform(Transform(rot, Vec3(0.5 * segmentalLength, 0, 0)));
+		posteriorDisplay->setScaleFactors(Vec3(segmentalDiam, segmentalLength, segmentalDiam));
+        posteriorBody->updDisplayer()->updGeometrySet().adoptAndAppend(posteriorDisplay);
         posteriorBody->updDisplayer()->setShowAxes(true);
-		posteriorBody->updDisplayer()->setScaleFactors(
-                Vec3(segmentalLength, segmentalDiam, segmentalDiam));
 
 		// Legs.
-		DisplayGeometry * anteriorLegsDisplay = 
-            new DisplayGeometry("box.vtp");
+		DisplayGeometry * anteriorLegsDisplay = new DisplayGeometry("box.vtp");
         anteriorLegsDisplay->setOpacity(0.5);
         anteriorLegsDisplay->setColor(Vec3(0.7, 0.7, 0.7));
-
-        anteriorLegs->updDisplayer()->updGeometrySet().adoptAndAppend(
-                anteriorLegsDisplay);
+		anteriorLegsDisplay->setTransform(Transform(Vec3(0.3 * legsLength, 0, 0)));
+		anteriorLegsDisplay->setScaleFactors(Vec3(legsLength, legsDiam, legsWidth));
+        anteriorLegs->updDisplayer()->updGeometrySet().adoptAndAppend(anteriorLegsDisplay);
         anteriorLegs->updDisplayer()->setShowAxes(true);
-		anteriorLegs->updDisplayer()->setScaleFactors(
-                Vec3(legsLength, legsDiam, legsWidth));
 
-		DisplayGeometry * posteriorLegsDisplay = 
-            new DisplayGeometry("box.vtp");
+		DisplayGeometry * posteriorLegsDisplay = new DisplayGeometry("box.vtp");
         posteriorLegsDisplay->setOpacity(0.5);
         posteriorLegsDisplay->setColor(Vec3(0.7, 0.7, 0.7));
-
-        posteriorLegs->updDisplayer()->updGeometrySet().adoptAndAppend(
-                posteriorLegsDisplay);
+		posteriorLegsDisplay->setTransform(Transform(Vec3(0.3 * legsLength, 0, 0)));
+		posteriorLegsDisplay->setScaleFactors(Vec3(legsLength, legsDiam, legsWidth));
+        posteriorLegs->updDisplayer()->updGeometrySet().adoptAndAppend(posteriorLegsDisplay);
         posteriorLegs->updDisplayer()->setShowAxes(true);
-		posteriorLegs->updDisplayer()->setScaleFactors(
-                Vec3(legsLength, legsDiam, legsWidth));
     }
 };
 
---
+++ modelTree.cpp
@@ -0,0 +1,345 @@
+
+#include "FelisCatusModeling.h"
+
+/**
+ * This creates a "tree" of cat models, starting with a "zero-
+ * degree-of-freedom" (DOF) base case and adding complexity (i.e.,
+ * DOFs) one step at a time. Each cat model consists of two cylindrical
+ * segments connected by a three-DOF (back flexion or 'hunch', spinal
+ * twist, and side bending or 'wag') joint. Each model restricts the
+ * DOFs of this joint to a different degree. In addition, some models
+ * includes legs; some are rigid, normal to the cat's underside, while
+ * others are "retractable," allowing the cat to change the effective
+ * moment of inertia of each of its halves.
+ * */
+
+class FelisCatusModel : public FelisCatusModeling
+{
+public:
+
+	enum LegsType {None, Rigid, Retract};
+
+    // member functions to allow for stepwise model creation
+	// NOTE: each 'add' function takes care of actuators if necessary
+	FelisCatusModel(string modelName, string fileName,
+		bool canTwist, bool canHunch, bool canWag, LegsType whichLegs);
+	void createBaseCase();
+    void addTwist();
+	void addHunch();
+	void addWag();
+	void addRigidLegs();
+	void addRetractLegs();
+
+private:
+
+	static const int maxTorque = 100; // N-m
+};
+
+int main(int argc, char *argv[])
+{
+    // 1-DOF Models
+	FelisCatusModel m1("Leland_twist", "feliscatus_twist.osim",
+		true, false, false, FelisCatusModel::None);
+	FelisCatusModel m2("Leland_hunch", "feliscatus_hunch.osim",
+		false, true, false, FelisCatusModel::None);
+	FelisCatusModel m3("Leland_wag", "feliscatus_wag.osim",
+		false, false, true, FelisCatusModel::None);
+	FelisCatusModel m4("Leland_twistRigidLegs", "feliscatus_twistRigidLegs.osim",
+		true, false, false, FelisCatusModel::Rigid);
+	FelisCatusModel m5("Leland_hunchRigidLegs", "feliscatus_hunchRigidLegs.osim",
+		false, true, false, FelisCatusModel::Rigid);
+	FelisCatusModel m6("Leland_wagRigidLegs", "feliscatus_wagRigidLegs.osim",
+		false, false, true, FelisCatusModel::Rigid);
+	FelisCatusModel m7("Leland_retractLegs", "feliscatus_retractLegs.osim",
+		false, false, false, FelisCatusModel::Retract);
+	
+	// 2-DOF Models
+	FelisCatusModel m8("Leland_twistHunch", "feliscatus_twistHunch.osim",
+		true, true, false, FelisCatusModel::None);
+	FelisCatusModel m9("Leland_twistWag", "feliscatus_twistWag.osim",
+		true, false, true, FelisCatusModel::None);
+	FelisCatusModel m10("Leland_hunchWag", "feliscatus_hunchWag.osim",
+		false, true, true, FelisCatusModel::None);
+	FelisCatusModel m11("Leland_twistHunchRigidLegs", "feliscatus_twistHunchRigidLegs.osim",
+		true, true, false, FelisCatusModel::Rigid);
+	FelisCatusModel m12("Leland_twistWagRigidLegs", "feliscatus_twistWagRigidLegs.osim",
+		true, false, true, FelisCatusModel::Rigid);
+	FelisCatusModel m13("Leland_hunchWagRigidLegs", "feliscatus_hunchWagRigidLegs.osim",
+		false, true, true, FelisCatusModel::Rigid);
+	FelisCatusModel m14("Leland_twistRetractLegs", "feliscatus_twistRetractLegs.osim",
+		true, false, false, FelisCatusModel::Retract);
+	FelisCatusModel m15("Leland_hunchRetractLegs", "feliscatus_hunchRetractLegs.osim",
+		false, true, false, FelisCatusModel::Retract);
+	FelisCatusModel m16("Leland_wagRetractLegs", "feliscatus_wagRetractLegs.osim",
+		false, false, true, FelisCatusModel::Retract);
+
+	// 3-DOF Models
+	FelisCatusModel m17("Leland_twistHunchWag", "feliscatus_twistHunchWag.osim",
+		true, true, true, FelisCatusModel::None);
+	FelisCatusModel m18("Leland_twistHunchWagRigidLegs", "feliscatus_twistHunchWagRigidLegs.osim",
+		true, true, true, FelisCatusModel::Rigid);
+	FelisCatusModel m19("Leland_twistHunchRetractLegs", "feliscatus_twistHunchRetractLegs.osim",
+		true, true, false, FelisCatusModel::Retract);
+	FelisCatusModel m20("Leland_twistWagRetractLegs", "feliscatus_twistWagRetractLegs.osim",
+		true, false, true, FelisCatusModel::Retract);
+	FelisCatusModel m21("Leland_hunchWagRetractLegs", "feliscatus_hunchWagRetractLegs.osim",
+		false, true, true, FelisCatusModel::Retract);
+
+	// 4-DOF Models
+	FelisCatusModel m22("Leland_twistHunchWagRetractLegs", "feliscatus_twistHunchWagRetractLegs.osim",
+		true, true, true, FelisCatusModel::Retract);
+
+    return EXIT_SUCCESS;
+};
+
+FelisCatusModel::FelisCatusModel(string modelName, string fileName,
+		bool canTwist, bool canHunch, bool canWag, LegsType whichLegs)
+{
+	makeModel(modelName, fileName);
+	
+	createBaseCase();
+	
+	if (canTwist) {
+		addTwist();
+	}
+
+	if (canHunch) {
+		addHunch();
+	}
+
+	if (canWag) {
+		addWag();
+	}
+
+	if (whichLegs == Rigid) {
+		addRigidLegs();
+	} else if (whichLegs == Retract) {
+		addRetractLegs();
+	}
+
+	cat.print(fileName);
+}
+
+void FelisCatusModel::createBaseCase()
+{
+    Body & ground = cat.getGroundBody();
+
+    // Connecting the anterior body to the ground via a custom joint. 
+	// Rotation is defined via YZX Euler angles, named yaw, pitch, and
+	// roll respectively. The important translation is in Y, the direction
+	// of gravity.	
+	Vec3 locGAInGround(0);
+    Vec3 orientGAInGround(0);
+    Vec3 locGAInAnterior(0);
+    Vec3 orientGAInAnterior(0);
+
+	SpatialTransform groundAnteriorST;
+	groundAnteriorST.updTransformAxis(0).setCoordinateNames(
+            Array<string>("yaw", 1));
+    groundAnteriorST.updTransformAxis(0).setAxis(Vec3(0, 1, 0));
+    groundAnteriorST.updTransformAxis(1).setCoordinateNames(
+            Array<string>("pitch", 1));
+    groundAnteriorST.updTransformAxis(1).setAxis(Vec3(0, 0, 1));
+    groundAnteriorST.updTransformAxis(2).setCoordinateNames(
+            Array<string>("roll", 1));
+    groundAnteriorST.updTransformAxis(2).setAxis(Vec3(1, 0, 0));
+    groundAnteriorST.updTransformAxis(3).setCoordinateNames(
+            Array<string>("tx", 1));
+    groundAnteriorST.updTransformAxis(3).setAxis(Vec3(1, 0, 0));
+    groundAnteriorST.updTransformAxis(4).setCoordinateNames(
+            Array<string>("ty", 1));
+    groundAnteriorST.updTransformAxis(4).setAxis(Vec3(0, 1, 0));
+    groundAnteriorST.updTransformAxis(5).setCoordinateNames(
+            Array<string>("tz", 1));
+    groundAnteriorST.updTransformAxis(5).setAxis(Vec3(0, 0, 1));
+
+    CustomJoint * groundAnterior = new CustomJoint("ground_anterior",
+            ground, locGAInGround, orientGAInGround,
+            *anteriorBody, locGAInAnterior, orientGAInAnterior,
+            groundAnteriorST);
+
+    CoordinateSet & groundAnteriorCS = groundAnterior->upd_CoordinateSet();
+    // yaw
+    double groundAnteriorCS0range[2] = {-Pi, Pi};
+    groundAnteriorCS[0].setRange(groundAnteriorCS0range);
+    groundAnteriorCS[0].setDefaultValue(0);
+    groundAnteriorCS[0].setDefaultLocked(false);
+    // pitch
+    double groundAnteriorCS1range[2] = {-Pi, Pi};
+    groundAnteriorCS[1].setRange(groundAnteriorCS1range);
+    groundAnteriorCS[1].setDefaultValue(0);
+    groundAnteriorCS[1].setDefaultLocked(false);
+    // roll
+    double groundAnteriorCS2range[2] = {-Pi, Pi};
+    groundAnteriorCS[2].setRange(groundAnteriorCS2range);
+    groundAnteriorCS[2].setDefaultValue(0);
+    groundAnteriorCS[2].setDefaultLocked(false);
+    // tx
+    double groundAnteriorCS3range[2] = {-1, 1};
+    groundAnteriorCS[3].setRange(groundAnteriorCS3range);
+    groundAnteriorCS[3].setDefaultValue(0);
+	groundAnteriorCS[3].setDefaultLocked(true);
+    // ty
+    double groundAnteriorCS4range[2] = {-1, 5};
+    groundAnteriorCS[4].setRange(groundAnteriorCS4range);
+    groundAnteriorCS[4].setDefaultValue(2);
+	groundAnteriorCS[4].setDefaultLocked(false);
+    // tz
+    double groundAnteriorCS5range[2] = {-1, 1};
+    groundAnteriorCS[5].setRange(groundAnteriorCS5range);
+    groundAnteriorCS[5].setDefaultValue(0);
+    groundAnteriorCS[5].setDefaultLocked(true);
+
+    // Connecting the anterior and posterior bodies via a custom joint. 
+	// Rotation is defined via ZYX Euler angles, named hunch, wag, and
+	// twist respectively.
+    Vec3 locAPInAnterior(0);
+    Vec3 orientAPInAnterior(0);
+    Vec3 locAPInPosterior(0);
+    Vec3 orientAPInPosterior(0);
+
+	SpatialTransform anteriorPosteriorST;
+	anteriorPosteriorST.updTransformAxis(0).setCoordinateNames(
+            Array<string>("hunch", 1));
+    anteriorPosteriorST.updTransformAxis(0).setAxis(Vec3(0, 0, 1));
+    anteriorPosteriorST.updTransformAxis(1).setCoordinateNames(
+            Array<string>("wag", 1));
+    anteriorPosteriorST.updTransformAxis(1).setAxis(Vec3(0, 1, 0));
+    anteriorPosteriorST.updTransformAxis(2).setCoordinateNames(
+            Array<string>("twist", 1));
+	anteriorPosteriorST.updTransformAxis(2).setAxis(Vec3(1, 0, 0));
+
+    CustomJoint * anteriorPosterior = new CustomJoint("anterior_posterior",
+            *anteriorBody, locAPInAnterior, orientAPInAnterior,
+            *posteriorBody, locAPInPosterior, orientAPInPosterior,
+			anteriorPosteriorST);
+
+    CoordinateSet & anteriorPosteriorCS = anteriorPosterior->upd_CoordinateSet();
+    // hunch
+    double anteriorPosteriorCS0range[2] = {-0.5 * Pi, 0.5 * Pi};
+    anteriorPosteriorCS[0].setRange(anteriorPosteriorCS0range);
+    anteriorPosteriorCS[0].setDefaultValue(0);
+    anteriorPosteriorCS[0].setDefaultLocked(true);
+	// wag
+    double anteriorPosteriorCS1range[2] = {-0.5 * Pi, 0.5 * Pi};
+    anteriorPosteriorCS[1].setRange(anteriorPosteriorCS1range);
+    anteriorPosteriorCS[1].setDefaultValue(0);
+    anteriorPosteriorCS[1].setDefaultLocked(true);
+	// twist
+    double anteriorPosteriorCS2range[2] = {-0.5 * Pi, 0.5 * Pi};
+    anteriorPosteriorCS[2].setRange(anteriorPosteriorCS2range);
+    anteriorPosteriorCS[2].setDefaultValue(0);
+    anteriorPosteriorCS[2].setDefaultLocked(true);
+
+    cat.addBody(anteriorBody);
+    cat.addBody(posteriorBody);
+}
+
+void FelisCatusModel::addTwist()
+{
+	// Unlock twist.
+	cat.updCoordinateSet().get("twist").setDefaultLocked(false);
+	
+	// Add twist actuator.
+	CoordinateActuator * twistAct = new CoordinateActuator("twist");
+    twistAct->setName("twist_actuator");
+	twistAct->setMinControl(-maxTorque);
+    twistAct->setMaxControl(maxTorque);
+	cat.addForce(twistAct);
+}
+
+void FelisCatusModel::addHunch()
+{
+	// Unlock hunch.
+	cat.updCoordinateSet().get("hunch").setDefaultLocked(false);
+	
+	// Add hunch actuator.
+	CoordinateActuator * hunchAct = new CoordinateActuator("hunch");
+	hunchAct->setName("hunch_actuator");
+	hunchAct->setMinControl(-maxTorque);
+    hunchAct->setMaxControl(maxTorque);
+	cat.addForce(hunchAct);
+}
+
+void FelisCatusModel::addWag()
+{
+	// Unlock wag.
+	cat.updCoordinateSet().get("wag").setDefaultLocked(false);
+	
+	// Add wag actuator.
+	CoordinateActuator * wagAct = new CoordinateActuator("wag");
+	wagAct->setName("wag_actuator");
+	wagAct->setMinControl(-maxTorque);
+    wagAct->setMaxControl(maxTorque);
+	cat.addForce(wagAct);
+}
+
+void FelisCatusModel::addRigidLegs()
+{
+	Vec3 locALegsInAnterior(-0.75 * segmentalLength, 0.5 * segmentalDiam, 0);
+    Vec3 orientALegsInAnterior(0);
+    Vec3 locALegsInLegs(0);
+    Vec3 orientALegsInLegs(0, 0, -0.5 * Pi);
+    WeldJoint * anteriorToLegs = new WeldJoint("anterior_legs",
+            *anteriorBody, locALegsInAnterior, orientALegsInAnterior,
+            *anteriorLegs, locALegsInLegs, orientALegsInLegs);
+
+	Vec3 locPLegsInPosterior(0.75 * segmentalLength, 0.5 * segmentalDiam, 0);
+    Vec3 orientPLegsInPosterior(0, Pi, 0);
+    Vec3 locPLegsInLegs(0);
+    Vec3 orientPLegsInLegs(0, 0, -0.5 * Pi);
+    WeldJoint * posteriorToLegs = new WeldJoint("posterior_legs",
+            *posteriorBody, locPLegsInPosterior, orientPLegsInPosterior,
+            *posteriorLegs, locPLegsInLegs, orientPLegsInLegs);
+
+	cat.addBody(anteriorLegs);
+	cat.addBody(posteriorLegs);
+}
+
+void FelisCatusModel::addRetractLegs()
+{
+	// Adding 1-DOF legs.
+	Vec3 locALegsInAnterior(-0.75 * segmentalLength, 0.5 * segmentalDiam, 0);
+    Vec3 orientALegsInAnterior(0);
+    Vec3 locALegsInLegs(0);
+    Vec3 orientALegsInLegs(0, 0, -0.5 * Pi);
+    PinJoint * anteriorToLegs = new PinJoint("anterior_legs",
+            *anteriorBody, locALegsInAnterior, orientALegsInAnterior,
+            *anteriorLegs, locALegsInLegs, orientALegsInLegs);
+    CoordinateSet & anteriorToLegsCS = anteriorToLegs->upd_CoordinateSet();
+    anteriorToLegsCS[0].setName("frontLegs");
+    double anteriorToLegsCS0range[2] = {-0.5 * Pi, 0.5 * Pi};
+    anteriorToLegsCS[0].setRange(anteriorToLegsCS0range);
+    anteriorToLegsCS[0].setDefaultValue(0);
+    anteriorToLegsCS[0].setDefaultLocked(false);
+
+	Vec3 locPLegsInPosterior(0.75 * segmentalLength, 0.5 * segmentalDiam, 0);
+    Vec3 orientPLegsInPosterior(0, Pi, 0);
+    Vec3 locPLegsInLegs(0);
+    Vec3 orientPLegsInLegs(0, 0, -0.5 * Pi);
+    PinJoint * posteriorToLegs = new PinJoint("posterior_legs",
+            *posteriorBody, locPLegsInPosterior, orientPLegsInPosterior,
+            *posteriorLegs, locPLegsInLegs, orientPLegsInLegs);
+    CoordinateSet & posteriorToLegsCS = posteriorToLegs->upd_CoordinateSet();
+    posteriorToLegsCS[0].setName("backLegs");
+    double posteriorToLegsCS0range[2] = {-0.5 * Pi, 0.5 * Pi};
+    posteriorToLegsCS[0].setRange(posteriorToLegsCS0range);
+    posteriorToLegsCS[0].setDefaultValue(0);
+    posteriorToLegsCS[0].setDefaultLocked(false);
+
+	cat.addBody(anteriorLegs);
+	cat.addBody(posteriorLegs);
+
+	// Adding leg actuators.
+	CoordinateActuator * frontLegsAct = new CoordinateActuator("frontLegs");
+	frontLegsAct->setName("frontLegs_actuator");
+	frontLegsAct->setMinControl(-maxTorque);
+    frontLegsAct->setMaxControl(maxTorque);
+	cat.addForce(frontLegsAct);
+    
+	CoordinateActuator * backLegsAct = new CoordinateActuator("backLegs");
+    backLegsAct->setName("backLegs_actuator");
+    backLegsAct->setMinControl(-maxTorque);
+    backLegsAct->setMaxControl(maxTorque);
+    cat.addForce(backLegsAct);
+}
