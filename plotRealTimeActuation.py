2a30dfe861602a9345b828dd87f115349a2926a3
Christopher Dembia <cld72@cornell.edu>
2013-05-24 14:43:42 -0700

    Worked on GUI actuation plotting tools.

--- plotRealTimeActuation.py
+++ plotRealTimeActuation.py
@@ -1,7 +1,8 @@
 # An OpenSim GUI script that creates a plotter window, and plots in it all the
 # actuation curves. The actuation curves are obtained via a serialized file
 # that contains a FunctionSet of all the Function's in the
-# PrescribedController.
+# PrescribedController. The FunctionSet file is read continually in case it's
+# being updated.
 #
 # The script is for use with the FelisCatus project for ME 485 at Stanford
 # University.
@@ -11,6 +12,8 @@
 # Authors: Chris Dembia, Sean Sketch
 # Date: 19 May 2013
 
+import time
+
 import org.opensim.utils as utils
 
 # Prompt user for the file containing FunctionSet.
@@ -18,14 +21,13 @@ filepath = utils.FileUtils.getInstance().browseForFilename(".xml",
         "Select a file containing a FunctionSet.")
         
 # Load the FunctionSet.
-fset = modeling.FunctionSet(filepath)
-
 plot = createPlotterPanel("Flip Actuation")
-
-crv = list()
-# Create a curve for each function in the set.
-for i in range(fset.getSize()):
-    crv.append(addFunctionCurve(plot, fset.get(i)))
-
-#plot.setYAxisLabel("Force (N) / Torque (Nm)")
-#plot.setXAxisLabel("time (s)")
+while True:
+    fset = modeling.FunctionSet(filepath)
+    
+    crv = list()
+    # Create a curve for each function in the set.
+    for i in range(fset.getSize()):
+        crv.append(addFunctionCurve(plot, fset.get(i)))
+    
+    time.sleep(1)
